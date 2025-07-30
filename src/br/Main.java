0592a0bdbb790f1fa61bae063805ae2a566201e6
Guilherme <ariel.gmachado@hotmail.com>
2025-07-30 11:00:24 -0300

    Realiza testes de validação na MAIN

--- src/br/Main.java
+++ src/br/Main.java
@@ -2,13 +2,14 @@ package br;
 
 public class Main {
   public static void main(String[] args) {
-    Produto produto1 = new Produto("Notebook", 2500.00, 10);
+    Produto produto1 = new Produto("Trena De Aço 5mts X 19mm Auto Trava Vonder Plus",
+        2779.99, 10);
     produto1.exibirResumo();
 
-    Produto produto2 = new Produto("Martelete", 470.00);
+    Produto produto2 = new Produto("Martelete Comb Makita Dhr202z 18v + 1bat 3ah + Carreg E Aces", 1779.99);
 
 
-    produto2.adicionarQuantidade(87);
+    produto2.adicionarQuantidade(145);
     produto2.vender(3);
     produto2.exibirResumo();
 
